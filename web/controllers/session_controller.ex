4c1fb469b180cee3b6348c85a9b0004f8250a75b
Kyle Scully <scullykns@gmail.com>
2016-01-07 21:28:36 -0800

    added session logic

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,4 +4,18 @@ defmodule Blogging.SessionController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"session" => session_params}) do
+	  case Blogging.Session.login(session_params, Blogging.Repo) do
+	    {:ok, user} ->
+	      conn
+	      |> put_session(:current_user, user.id)
+	      |> put_flash(:info, "Logged in")
+	      |> redirect(to: "/")
+	    :error ->
+	      conn
+	      |> put_flash(:info, "Wrong email or password")
+	      |> render("new.html")
+	  end
+end
 end
